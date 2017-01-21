66a3ef37b6243111869921267862328256dfee8a
Luke Stowe <lukestowe@Lukes-MacBook-Pro.local>
2017-01-21 16:17:42 +0000

    Messing around with dropdown choices

--- app/index.js
+++ app/index.js
@@ -1,15 +1,47 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ShowList = React.createClass({
+  render: function(){
+      var listItems = this.props.options.map(function(optionValue){
+        return  <option value="{{optionValue}}">{optionValue}</option>;
+      });
+      return (
+
+          <select name="values">
+            {listItems}
+          </select>
+      )
+    }
+
+});
+
+var Input = React.createClass({
   render: function () {
     return (
-      <div> Hello React Program </div>
+    <div>
+      <h2>{this.props.name}</h2>
+      <ShowList options={this.props.options}/>
+      <input type="submit" value="Submit"/>
+  </div>
     )
   }
 });
+var InputPanel = React.createClass({
+  render: function () {
+    var options = ['80','110','130']
+    var optionsMa = ['50','100','500']
+    return (
+      <div>
+      <Input name="kV" options = {options}/>
+      <Input name="ma" options = {optionsMa}/>
+      </div>
+    )
+  }
+
+});
 
 ReactDOM.render(
-  <HelloWorld />,
+  <InputPanel/>,
   document.getElementById('app')
 );
